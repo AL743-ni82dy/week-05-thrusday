b5f2e7ef01e2436ed85d4ad22736e68a33d5ebe4
Alvin <fulmar38251@letterboxes.org>
2020-07-02 20:04:04 -0400

    Fill in body of all function

--- all.js
+++ all.js
@@ -1,6 +1,17 @@
+const anarray = [true,false]
 function all(arr) {
+	
 	// Loop through every item in the array
+	for( let x = 0; x < arr.length; x++ ) {
+		if ( !arr[x] ) { 
+			return false 
+		} else if ( x >= arr.length ) {
+			return true
+		}
+	}
+}
+console.log(all(anarray))
 	// If the item evaluates to false, return false
 
 	// If the loop ends, return true
-}
+
