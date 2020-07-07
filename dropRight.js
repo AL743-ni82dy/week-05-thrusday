1d764056fa82a53abc397e4c5ab700671ee9f07f
Alvin <fulmar38251@letterboxes.org>
2020-07-07 08:22:28 -0400

    write function for dropRight and flatten

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,10 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
-
-	// If the loop ends, return true
+function dropRight(arr, n) {
+	// if n is larger than array length return []
+	if ( n >= arr.length ) {
+		return []
+	}
+	// Use n to count in from the end to get the index
+	return arr.slice(0,(arr.length - n))
+	// return a slice of the array using the index as the end
 }
+console.log(dropRight(['d','e','f','g','h','i'], 3))
--- flatten.js
+++ flatten.js
@@ -1,9 +1,28 @@
 function flatten(arr) {
 	// Create a new array
+	let newArr = []
 
 	// Loop through every item in the array
 	// If the item is an array, add its items to the new array
 	// If not add the item itself to the new array
+	for(let x = 0; x < arr.length; x++) {
+		let tmp = arr[x]
+		if (Array.isArray(tmp)) {
+			for(let y = 0; y < tmp.length; y++) {
+				newArr.push(tmp[y])
+			}
+
+		} else {
+			newArr.push(tmp)
+		}
+	}
 
 	// return the new array
+	return newArr
 }
+
+console.log( flatten( ['a','b','c','d']))
+console.log( flatten( ['a','b',['c','d'],'e','f'] ) )
+console.log( flatten( ['a','b',['c','d','e'],'f'] ) )
+console.log( flatten( ['a',['b','c'],'d',['e','f']] ) )
+
